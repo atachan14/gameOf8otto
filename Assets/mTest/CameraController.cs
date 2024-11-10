c223e5c5aab384ed0bfdab5e6128d96a9e1652ea
WIN-1Q8DR817JKE\user <aklius14pg@gmail.com>
2024-11-11 02:55:24 +0900

    雑仮マップと、ついでにカメラがプレイヤーのy軸をゆっくり追従するようにしました。

--- Assets/mTest/CameraController.cs
+++ Assets/mTest/CameraController.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    float cameraSpeed = 10/60f;
+    public float cameraSpeed = 10 / 60f;
+    public GameObject player;
+    public float yOffset;
+    public float yDefault;
+    public float yUpAria;
+    public float yDownAria;
+
+    Vector3 nextPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right * cameraSpeed;
+        nextPos.x += cameraSpeed;
+
+        //if (player.transform.position.y > yDefault + yUpAria)
+        //{
+            nextPos.y = Mathf.Lerp(transform.position.y, player.transform.position.y + yOffset, 0.1f);
+        //}
+        //else if (player.transform.position.y < yDefault - yDownAria)
+        //{
+        //    nextPos.y = Mathf.Lerp(player.transform.position.y, yUpAria + yOffset,0.1f);
+        //}
+        //else
+        //{
+        //    nextPos.y = yDefault;
+        //}
+        transform.position = nextPos;
     }
 }
