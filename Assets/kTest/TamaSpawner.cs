52b02ed11d32888944d173e133dadf99db8241e5
kowi1700 <kou.and.more@gmail.com>
2024-11-11 12:43:57 +0900

    アニメーション練習。 posMarkerとランダムな赤い弾。

--- Assets/kTest/TamaSpawner.cs
+++ Assets/kTest/TamaSpawner.cs
@@ -17,9 +17,13 @@ public class TamaSpawner : MonoBehaviour
 
 
     void GeneratePrefab()
-    {
-        Vector3 spawnPosition = mainCamera.transform.position + mainCamera.transform.forward * spawnDistance;
+    {   
+        float randomY = Random.Range(mainCamera.ViewportToWorldPoint(new Vector3(0, 0, spawnDistance)).y,
+                                     mainCamera.ViewportToWorldPoint(new Vector3(0, 1, spawnDistance)).y);
         
+        Vector3 spawnPosition = mainCamera.transform.position + mainCamera.transform.forward * spawnDistance;
+        spawnPosition.y = randomY + 1f;
+
         Instantiate(prefab, spawnPosition, Quaternion.identity);
     }
 }
